3284a5d16957de45d87c1c6cd4c7b3de3614f990
plutos28 <gamevictor28@gmail.com>
2024-09-03 13:27:37 +0300

    add in the nav

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 
 import App from './App.jsx'
-import HomePage from './routes/HomePage.jsx'
+import HomePage from './routes/homePage.jsx'
 import './css/output.css'
 
 const router = createBrowserRouter([
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'jetbrains': ['"JetBrains Mono"', 'monospace'],
+      },
+      colors: {
+        hitchBlue: '#007AFF',
+        hitchUnselectedTab: '#999999',
+        hitchSecondaryLabel: '#8A8A8E',
+        hitchBorder: 'rgba(0, 0, 0, 0.2)'
+      },
+    },
   },
   plugins: [],
 }
